0c6ed383ccdb55bbbeab63f98cfd7982c502bcd2
sweety jain <sweetie.jn@gmail.com>
2012-11-04 00:09:05 +0530

    Layout and login
    Layout page and login page with dummy User ID -admin and password-admin
    is created.image and password Textbox css is pending.

---
+++ Code/MVC Version/ASTMMVC/MvcApplication2/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication2.Models;
+using System.Web.Security;
+namespace MvcApplication2.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        
+        public ActionResult Login(AccountModel.LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid && model.UserName =="admin" && model.Password=="admin")
+            {
+                FormsAuthentication.SetAuthCookie(model.UserName,false);
+                
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Course");
+                }
+                
+            }
+
+            // If we got this far, something failed, redisplay form
+            
+            return View(model);
+        }
+
+    }
+}
